48e53c4ded0b8d3de3dc9014a5e49003d2763a84
briws <briws@pm.me>
2021-04-09 20:03:46 +0300

    02 - progress

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
 
-type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
+export type AffairsPropsType = { // need to fix any
+    data: Array<AffairType>
+    setFilter: FilterType
     deleteAffairCallback: any
 }
 
