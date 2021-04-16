09d832a4a49e86b48925ce521411f53278aab175
briws <briws@pm.me>
2021-04-16 19:59:41 +0300

    03 - homework complete

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,23 +1,24 @@
-import React, {ChangeEvent} from 'react'
+import React, {ChangeEvent, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
+    name: string
     setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    addUserKeyCallback: (e: KeyboardEvent<HTMLInputElement>) => void
+    addUser: () => void
+    error: string
+    totalUsers: number
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+    {name, setNameCallback, addUserKeyCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = (error) ? s.error : ''
 
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
+            <input value={name} onChange={setNameCallback} onKeyPress={addUserKeyCallback} className={inputClass}/>
             <span>{error}</span>
             <button onClick={addUser}>add</button>
             <span>{totalUsers}</span>
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,10 +1,10 @@
-import React, {ChangeEvent, useState} from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
 import {UserType} from './HW3';
 
 type GreetingContainerPropsType = {
-    users: Array<UserType>
-    addUserCallback: (name: string) => void
+  users: Array<UserType>
+  addUserCallback: (name: string) => void
 }
 
 // более простой и понятный для новичков
@@ -13,27 +13,43 @@ type GreetingContainerPropsType = {
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<string>('')
-    const [error, setError] = useState<string>('')
+  const [name, setName] = useState<string>('')
+  const [error, setError] = useState<string>('')
 
-    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        setName(e.currentTarget.value)
+  const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.currentTarget.value)
+    if (e.currentTarget.value.trim() !== '') setError('')
+  }
+
+  const addUserKeyCallback = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      addUser()
     }
-    const addUser = () => {
-        alert(`Hello  !`) // need to fix
+  }
+
+  const addUser = () => {
+    if (name.trim() === '') {
+      setError('name is ruquire!')
+    } else {
+      alert(`Hello ${name.trim()} !`)
+      addUserCallback(name.trim());
     }
+  }
 
-    const totalUsers = 0 // need to fix
 
-    return (
-        <Greeting
-            name={name}
-            setNameCallback={setNameCallback}
-            addUser={addUser}
-            error={error}
-            totalUsers={totalUsers}
-        />
-    )
+
+  const totalUsers = users.length
+
+  return (
+    <Greeting
+      name={name}
+      setNameCallback={setNameCallback}
+      addUser={addUser}
+      addUserKeyCallback={addUserKeyCallback}
+      error={error}
+      totalUsers={totalUsers}
+    />
+  )
 }
 
 export default GreetingContainer
