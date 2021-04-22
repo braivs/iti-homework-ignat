391a61f3215bd504fa3cf3785c3c2fc1f58dc223
briws <briws@pm.me>
2021-04-22 22:06:24 +0300

    04 - 3/7

--- src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
+++ src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
@@ -39,7 +39,7 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
     }
 
     const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
-    const finalInputClassName = `${s.errorInput} ${className}` // need to fix with (?:) and s.superInput
+    const finalInputClassName = `${(error) ? s.errorInput : ''} ${className}`
 
     return (
         <>
@@ -51,7 +51,6 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
 
                 {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
             />
-            {error && <span className={finalSpanClassName}>{error}</span>}
         </>
     )
 }
