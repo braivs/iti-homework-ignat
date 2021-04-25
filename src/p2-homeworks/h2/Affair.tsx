c09def909a00d48de8ac44b167c975fc7873aad9
briws <briws@pm.me>
2021-04-25 18:32:01 +0300

    02 - styling progress

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {AffairType} from './HW2';
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton';
+import s from './Affair.module.scss'
 
 type AffairPropsType = {
     // key не нужно типизировать
@@ -10,12 +11,15 @@ type AffairPropsType = {
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {props.deleteAffairCallback(props.affair._id)}// need to fix
+    let priorityClass;
+    if (props.affair.priority === 'high') priorityClass = `${s.high}`
+    if (props.affair.priority === 'middle') priorityClass = `${s.middle}`
 
     return (
-        <div>
-          {props.affair.name}
-          {`[${props.affair.priority}]`}
-          <SuperButton onClick={deleteCallback}>X</SuperButton>
+        <div className={s.affairContainer}>
+            <div>{props.affair.name}</div>
+            <div className={priorityClass}>{`[${props.affair.priority}]`}</div>
+            <SuperButton onClick={deleteCallback} className={s.delButton}>X</SuperButton>
         </div>
     )
 }
