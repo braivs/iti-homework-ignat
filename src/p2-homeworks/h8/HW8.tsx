66bc6a78215b8f4ca8e48a361eaec5513989ebae
BriWS <briws@pm.me>
2021-06-05 13:45:01 +0300

    08 - complete

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,50 +1,53 @@
 import React, {useState} from 'react'
-import {homeWorkReducer, SortNameUpAC} from './bll/homeWorkReducer'
+import {CheckAC, homeWorkReducer, SortNameDownAC, SortNameUpAC} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {UserType} from './bll/tests/homeWorkReducer.test';
+import s from './HW8.module.scss'
 
 // export type UserType =
 
 const initialPeople = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+  {_id: 0, name: 'Кот', age: 3},
+  {_id: 1, name: 'Александр', age: 66},
+  {_id: 2, name: 'Коля', age: 16},
+  {_id: 3, name: 'Виктор', age: 44},
+  {_id: 4, name: 'Дмитрий', age: 40},
+  {_id: 5, name: 'Ирина', age: 55},
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<Array<UserType>>(initialPeople)
+  const [people, setPeople] = useState<Array<UserType>>(initialPeople)
 
-    // need to fix any
-    const finalPeople = people.map((p: UserType) => (
-        <div key={p._id}>
-          <span>{p.name}</span>
-          <span>{p.age}</span>
-        </div>
-    ))
+  const finalPeople = people.map((p: UserType) => (
+    <div className={s.container} key={p._id}>
+      <span>{p.name}</span>
+      <span>{p.age}</span>
+    </div>
+  ))
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, SortNameUpAC()))
+  const sortUp = () => setPeople(homeWorkReducer(initialPeople, SortNameUpAC()))
+  const sortDown = () => setPeople(homeWorkReducer(initialPeople, SortNameDownAC()))
+  const check18 = () => setPeople(homeWorkReducer(initialPeople, CheckAC()))
 
-    return (
-        <div>
-            <hr/>
-            homeworks 8
+  return (
+    <div>
+      <hr/>
+      homeworks 8
 
-            {/*should work (должно работать)*/}
-            {finalPeople}
+      {/*should work (должно работать)*/}
+      {finalPeople}
+      <div className={s.buttonsContainer}>
+        <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
+        <div><SuperButton onClick={sortDown}>sort down</SuperButton></div>
+        <div><SuperButton onClick={check18}>check 18</SuperButton></div>
+      </div>
 
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativePeople/>*/}
-            <hr/>
-        </div>
-    )
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativePeople/>*/}
+      <hr/>
+    </div>
+  )
 }
 
 export default HW8
