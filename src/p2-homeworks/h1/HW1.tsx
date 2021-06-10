3e7d2f38f7d0ede0a5bed37d5c9c985dc6fd2f44
BriWS <briws@pm.me>
2021-06-10 18:40:47 +0300

    01 partial correction according to the answer

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -3,9 +3,9 @@ import Message from './Message';
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Artem',
-    message: 'npm start нажимал?',
-    time: '20:00',
+    name: 'Some Name',
+    message: 'some text',
+    time: '22:00',
 }
 
 // type
@@ -18,12 +18,12 @@ function HW1() {
 
             {/*should work (должно работать)*/}
 
-            {<Message
+            <Message
                 avatar={messageData.avatar}
                 name={messageData.name}
                 message={messageData.message}
                 time={messageData.time}
-            />}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -12,15 +12,15 @@ function Message(props: messageDataType) {
   return (
     <div className={s.message}>
       <img src={props.avatar} alt="" className={s.avatar}/>
-      <svg className={s.messageCorner} xmlns="http://www.w3.org/2000/svg">
-        <path fill="#1A1C27" d="M 0 20 A 20 20, 0, 0, 0, 20 0 L 20 20 Z"></path>
-      </svg>
-      <div className={s.messageBody}>
+
+      <div className={s.angel}></div>
+
+      <div className={s.content}>
         <div className={s.leftPart}>
-          <div className={s.messageName}>{props.name}</div>
-          <div className={s.messageText}>{props.message}</div>
+          <div className={s.name}>{props.name}</div>
+          <div className={s.text}>{props.message}</div>
         </div>
-        <div className={s.messageTime}>{props.time}</div>
+        <div className={s.time}>{props.time}</div>
 
       </div>
     </div>
